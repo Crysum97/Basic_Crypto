2daddad811ccd14b0ab6cbb1156bf755b1237056
marvin.loth <marvin.loth@stud.th-luebeck.de>
2024-01-27 15:32:58 +0100

    Encryption preview works

--- src/main/java/controller/EncryptPanelController.java
+++ src/main/java/controller/EncryptPanelController.java
@@ -1,6 +1,11 @@
 package controller;
 
+import engine.CryptoEngine;
+import util.CryptoException;
 import view.EncryptPanel;
+
+import java.io.File;
+import java.util.Locale;
 
 public class EncryptPanelController extends FileController {
     EncryptPanel view;
@@ -11,5 +16,32 @@ public class EncryptPanelController extends FileController {
 
     public void onOpenFile() {
         openFile().ifPresent(file -> view.getKeyPathTextField().setText(file.getAbsolutePath()));
+    }
+
+    public void encryptCaesar(CryptoEngine engine,String input, int key) {
+        try {
+            String encrypted = engine.encrypt(input, key);
+            view.getPreviewTextArea().setText(encrypted);
+        } catch (CryptoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void encryptMatrix(CryptoEngine engine, String input) {
+        try {
+            String encrypted = engine.encrypt(input);
+            view.getPreviewTextArea().setText(encrypted);
+        } catch (CryptoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void encryptVigerene(CryptoEngine engine, String input, String key) {
+        try {
+            String encrypted = engine.encrypt(input, readFromFile(new File(key)).toUpperCase().replaceAll("[^A-Z]", ""));
+            view.getPreviewTextArea().setText(encrypted);
+        } catch (CryptoException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/view/EncryptPanel.java
+++ src/main/java/view/EncryptPanel.java
@@ -1,10 +1,11 @@
 package view;
 
 import controller.EncryptPanelController;
+import iterative.Matrix;
+import iterative.Vigerene;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.Objects;
 
 public class EncryptPanel {
     private JButton backButton;
@@ -21,6 +22,17 @@ public class EncryptPanel {
         this.controller = new EncryptPanelController(this);
         backButton.addActionListener(e -> parent.popLastPanel());
         openButton.addActionListener(e -> controller.onOpenFile());
+        encryptButton.addActionListener(e -> {
+            String input = parent.input.getContentContainer().getText();
+            String keyField = keyTextField.getText();
+            switch (Objects.requireNonNull(methodComboBox.getSelectedItem()).toString()) {
+                case "Caesar 1" -> controller.encryptCaesar(new functional.Caesar(), input, Integer.parseInt(keyField));
+                case "Caesar 2" -> controller.encryptCaesar(new iterative.Caesar(), input, Integer.parseInt(keyField));
+                case "Matrix" -> controller.encryptMatrix(new Matrix(), input);
+                case "Vigerene" -> controller.encryptVigerene(new Vigerene(), input, keyField);
+                default -> System.out.println("Shouldn't be here...");
+            }
+        });
     }
 
     public JPanel getRoot() {
@@ -29,5 +41,9 @@ public class EncryptPanel {
 
     public JTextField getKeyPathTextField() {
         return this.keyTextField;
+    }
+
+    public JTextArea getPreviewTextArea() {
+        return this.previewTextArea;
     }
 }
